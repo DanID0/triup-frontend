f2ebaacb213bfcf8f4977dd9f0c7bdb4d9cf2027
Dani <damians.geidels2@gmail.com>
2026-01-19 20:14:12 +0200

    First commit, added some files, folders, route for testing purposes.

---
+++ src/app/Pages/workboard/workboard/workboard.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workboard',
+  imports: [],
+  templateUrl: './workboard.html',
+  styleUrl: './workboard.css',
+})
+export class Workboard {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './Pages/home-page/home-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //Kogda zdelajesh layout ispolzuj eto kak example
+    //path: '', (path ostavj pustim)
+    //component: LayoutPageComponent ( ili kak tam  ego nazavu),
+    //children: [ ( sjuda vstavlaj vse komponenti gde etot samij layout budet usaca)
+    {
+        path: '',
+        loadComponent: () =>
+          import('./Pages/home-page/home-page').then(
+            (m) => m.HomePage
+          ),
+      },
+];
